ccc551a8da741c72ef4bfa952567450e1d741e32
Daniel Keller <danielkeller@Daniels-MacBook-Pro-3.local>
2019-01-02 15:33:27 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,22 @@
-console.log("Up and Running!");
+var cards = ["queen", "queen", "king", "king"];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cardOne = cards[0];
+cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped" + " " + cardOne);
+console.log("User flipped" + " " + cardTwo);
+
+
+if (cardsInPlay.length === 2) {
+    } if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match"); 
+    } else { 
+        alert("Sorry, try again");
+    }
+    
+console.log(cardsInPlay);
