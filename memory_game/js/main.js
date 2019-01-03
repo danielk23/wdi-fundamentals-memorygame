a9700fe883969e82bce1432aecef2cfb483c5830
Daniel Keller <danielkeller@Daniels-MacBook-Pro-3.local>
2019-01-03 17:13:50 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,28 @@
+
+
+
+
+
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-cardOne = cards[0];
-cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+var checkForMatch = function() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!"); 
+        } else { 
+        console.log("Sorry, try again.");
+        }
+};
 
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardTwo);
 
 
-if (cardsInPlay.length === 2) {
-    } if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match"); 
-    } else { 
-        alert("Sorry, try again");
-    }
+var flipCard = function(cardId) {
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
     
-console.log(cardsInPlay);
+    checkForMatch();   
+};
+
+flipCard(3);
