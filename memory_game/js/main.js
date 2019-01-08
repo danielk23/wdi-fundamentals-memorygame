44709e113cd0ab31b848a71bb1ab1602fbd92fb3
Daniel Keller <danielkeller@Daniels-MacBook-Pro-3.local>
2019-01-08 15:43:40 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -29,68 +29,32 @@ var cardsInPlay = [];
 
 var checkForMatch = function() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!"); 
+        alert("You found a match!")
         } else { 
         alert("Sorry, try again.");
         }
 };
 
 
+var flipCard = function() {
+        var cardId = this.getAttribute('data-id');
+        var currentCard = cards[cardId];
+        this.setAttribute('src', currentCard.cardImage);
+        cardsInPlay.push(cards.rank);
+        if (cardsInPlay.length === 2) {
+		checkForMatch();
+        }
+};
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 
-var flipCard = function(cardId) {
-    console.log("User flipped " + cards[cardId].rank);
-    cardsInPlay.push(cards[cardId].rank);
-    
-    console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
-    
-    
-    checkForMatch();   
-
-   
+var createBoard = function() {
+        for (var i = 0; i < cards.length; i++) {
+                var cardElement = document.createElement('img');
+                cardElement.setAttribute('src', 'images/back.png');
+                cardElement.setAttribute('data-id', i);
+                cardElement.addEventListener('click', flipCard);
+                document.getElementById('game-board').appendChild(cardElement);
+        }
 };
 
-
-flipCard(0);
-flipCard(2);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var cards = ["queen", "queen", "king", "king"];
-
-// var cardsInPlay = [];
-
-
-// var checkForMatch = function() {
-//     if (cardsInPlay[0] === cardsInPlay[1]) {
-//         console.log("You found a match!"); 
-//         } else { 
-//         console.log("Sorry, try again.");
-//         }
-// };
-
-
-
-// var flipCard = function(cardId) {
-//     console.log("User flipped " + cards[cardId]);
-//     cardsInPlay.push(cards[cardId]);
-    
-//     checkForMatch();   
-// };
-
-// flipCard(3);
+createBoard();
